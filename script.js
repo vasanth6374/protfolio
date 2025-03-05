a9cb709421013296f4b5cdd8c73fc6e57574f745
vasanth6374 <148033857+vasanth6374@users.noreply.github.com>
2025-03-05 21:57:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle Menu for Mobile
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
